8d087b067a04de8bf8744bbd826f8f9aae9b2c73
Catherine Kwak <catherine.kwak@capterra.com>
2021-01-21 21:38:33 -0700

    Clear out everything

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.portfolio-item').hover(function() {
-      $(this).find('.portfolio-title').fadeIn(300);
-  }, function() {
-      $(this).find('.portfolio-title').fadeOut(100);
-  });
-});
